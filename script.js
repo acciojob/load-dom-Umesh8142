e5a689414434bf3fb7ea411bc63636a0dd84fb61
N UMESH <102941752+Umesh8142@users.noreply.github.com>
2023-10-19 11:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 //your JS code here. If required.
-	const h1 =document.createElement("h1");
-	h1.innerText="DOM load success"
-	document.body.appendChild(h1)
+document.addEventListener('DOMContentLoaded', function() {
+            // Get the body element
+            var body = document.body;
+
+            // Clear the body (remove any existing content)
+            body.innerHTML = '';
+
+            // Create a new paragraph element
+            var paragraph = document.createElement('p');
+
+            // Set the text content of the paragraph
+            paragraph.textContent = 'DOM load success';
+
+            // Append the paragraph to the body
+            body.appendChild(paragraph);
+        });
